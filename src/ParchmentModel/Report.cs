c0d87ab98edd3900022cc85d1973ecb7298abea5
Simon Cropp <simon.cropp@gmail.com>
2026-05-12 16:53:11 +1000

    docs

--- src/ParchmentModel/Report.cs
+++ src/ParchmentModel/Report.cs
@@ -3,30 +3,30 @@ namespace ParchmentModel;
 #region ReportModel
 public class ReportContext
 {
-    public required Report Report { get; init; }
+    public required Report Report;
 }
 
 public class Report
 {
-    public required string Title { get; init; }
-    public required string Author { get; init; }
-    public required Date Date { get; init; }
-    public required string Summary { get; init; }
-    public required IReadOnlyList<Finding> Findings { get; init; }
-    public required IReadOnlyList<ActionItem> Actions { get; init; }
-    public required bool HasRisks { get; init; }
+    public required string Title;
+    public required string Author;
+    public required Date Date;
+    public required string Summary;
+    public required IReadOnlyList<Finding> Findings;
+    public required IReadOnlyList<ActionItem> Actions;
+    public required bool HasRisks;
 }
 
 public class Finding
 {
-    public required string Area { get; init; }
-    public required string Status { get; init; }
-    public required string Owner { get; init; }
+    public required string Area;
+    public required string Status;
+    public required string Owner;
 }
 
 public class ActionItem
 {
-    public required string Title { get; init; }
-    public required string Detail { get; init; }
+    public required string Title;
+    public required string Detail;
 }
 #endregion
